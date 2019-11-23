d2d5b1d279c7692a4ea4cc1beafc0b89f17fc7bb
Juan José Arboleda <soyjuanarbol@gmail.com>
2019-11-23 13:59:22 -0500

    Updates

---
+++ precache-manifest.87b29adf594d345672b676e30aa7ee8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aeacb9428b8b1b0c1e75530080114c5",
+    "url": "/cacerola/index.html"
+  },
+  {
+    "revision": "45a2dd11e78c2b6f947d",
+    "url": "/cacerola/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f7f097f6e7b2e631637",
+    "url": "/cacerola/static/js/2.888254f9.chunk.js"
+  },
+  {
+    "revision": "45a2dd11e78c2b6f947d",
+    "url": "/cacerola/static/js/main.0fb911ba.chunk.js"
+  },
+  {
+    "revision": "cab6d2425c603be3239f",
+    "url": "/cacerola/static/js/runtime-main.f7e99d5b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cacerola/static/media/logo.25bf045c.svg"
+  }
+]);
